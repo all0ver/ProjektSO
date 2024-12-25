56253db114756657d5b4f0e26f73c39bea1b9b29
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-12-25 18:30:47 +0000

    clean up

--- main.c
+++ main.c
@@ -24,100 +24,24 @@
 // msget, msgsnd, msgrcv
 #include <sys/msg.h>
 
-void procFunctions() {
-
+void P2()
+{
+  // process 2 -> Mateusz
+  printf("\nProcess 2");
+  // get data from proc1 -> shm i sem
+  char lineFromProc1[1024];
+  strcpy(lineFromProc1, "for testing");
+  int lineLength = strlen(lineFromProc1);
+  printf("\nLine lenght: %i", lineLength);
+  // send lineLength to proc3 -> msg?
 }
 
 int main(int argc, char *argv[])
 {
-  // assuming that every process is a child process
-  pid_t processes[3];
-
-  // creating processes
-  // first process
-  processes[0] = fork();
-
-  switch (processes[0])
-  {
-  case -1:
-    printf("\nfork() error->proc1");
-    return 1;
-    break;
-  case 0:
-    // process 1 -> Kamil
-    printf("\nProcess 1");
-    exit(0);
-    break;
-
-  default:
-    // main process
-    processes[1] = fork();
-    switch (processes[1])
-    {
-    case -1:
-      printf("\nfork() error->proc2");
-      return 1;
-      break;
-    case 0:
-      // process 2 -> Mateusz
-      printf("\nProcess 2");
-      // get data from proc1 -> shm i sem
-      char lineFromProc1[1024];
-      strcpy(lineFromProc1, "for testing");
-      int lineLength = strlen(lineFromProc1);
-      printf("\nLine lenght: %i", lineLength);
-      // send lineLength to proc3 -> msg?
-      exit(0);
-      break;
-    default:
-      // main process
-      // creating third process
-      processes[2] = fork();
-      switch (processes[2])
-      {
-      case -1:
-        printf("\nfork() error->proc3");
-        return 1;
-        break;
-      case 0:
-        // process 3 -> Paweł
-        printf("\nProcess 3");
-        exit(0);
-        break;
-      default:
-        break;
-      }
-      break;
-    }
-    break;
-  }
-
-  // waitint for all process to finish
+  // wait for all process to finish
   for (int i = 0; i < 3; i++)
   {
     wait(NULL);
-  }
-
-  // main menu
-  char select;
-  while (1)
-  {
-    printf("\nSelect: ");
-    scanf(" %c", &select);
-    switch (select)
-    {
-    case '1':
-      printf("\nData from file");
-      break;
-    case '2':
-      printf("\nData from stdin");
-      break;
-    default:
-      printf("\nInvalid choice");
-      break;
-    }
-  }
-
   printf("\n");
   return 0;
 }
